53f7b2d98137ba83804448123b0324b5113dbe17
shin yoon ji-jdbc <tlsdbswl@naver.com>
2019-03-22 14:57:04 +0900

    first commit

---
+++ src/main/java/com/yoonji/jdbc/spring/Greeter.java
@@ -0,0 +1,11 @@
+package com.yoonji.jdbc.spring;
+
+public class Greeter {
+	public Greeter() {
+		System.out.println("Greeter instance created.");
+	}
+	
+	public String greet() {
+		return "Hello, World!";
+	}
+}
