f622d9e78b7e392d88aa4e7dd81fb8732f13e6d8
shin yoon ji-jdbc <tlsdbswl@naver.com>
2019-04-05 14:53:57 +0900

    add src

---
+++ src/main/java/com/yoonji/article_1/ArticleService.java
@@ -0,0 +1,72 @@
+package com.yoonji.article_1;
+import java.util.List;
+
+/**
+ * articleDao를 사용해서 글 목록, 조회, 추가, 수정, 삭제를 한다.
+ * 
+ * @author Jacob
+ */
+public class ArticleService {
+
+	ArticleDao articleDao;
+
+	public void setArticleDao(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+
+	/**
+	 * 글 목록
+	 */
+	public void listArticles() {
+		List<Article> articles = articleDao.listArticles();
+		System.out.println(articles);
+	}
+
+	/**
+	 * 글 조회
+	 */
+	public void getArticle() {
+		Article article = articleDao.getArticle("3");
+		System.out.println(article);
+	}
+
+	/**
+	 * 글 등록
+	 */
+	public void addArticle() {
+		Article article = new Article();
+		article.setTitle("존경하는 조원석 교수님");
+		article.setContent(
+				"교수님 오늘처럼 진도 이렇게 나가면 참 좋을 거 같아용^^ 교수님 멋져용 ㅎㅎ");
+		article.setUserId("1");
+		article.setName("신윤지");
+		if (articleDao.addArticle(article) > 0)
+			System.out.println("글을 추가했습니다.");
+		else
+			System.out.println("글을 추가하지 못했습니다.");
+	}
+
+	/**
+	 * 글 수정
+	 */
+	public void updateArticle() {
+		Article article = new Article();
+		article.setArticleId("7");
+		article.setTitle("This is modified title.");
+		article.setContent("This is modified content");
+		if (articleDao.updateArticle(article) > 0)
+			System.out.println("글을 수정했습니다.");
+		else
+			System.out.println("글을 수정하지 못했습니다.");
+	}
+
+	/**
+	 * 글 삭제
+	 */
+	public void deleteArticle() {
+		if (articleDao.deleteArticle("8") > 0)
+			System.out.println("글을 삭제했습니다.");
+		else
+			System.out.println("글을 삭제하지 못했습니다.");
+	}
+}
